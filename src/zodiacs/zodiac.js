345051601b525b5aec11984f09034fedeb491973
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-10 18:35:28 -0800

    added styling and created cards

--- src/zodiacs/zodiac.js
+++ src/zodiacs/zodiac.js
@@ -2,6 +2,11 @@ import './zodiac.css';
 
 export default function Zodiac(props) {
   return (<div className='zodiac'>
-    <><img src={`${process.env.PUBLIC_URL}/symbols/${props.name}.png`} /><h2 className="name">{props.name}</h2><h3>{props.symbol}</h3><p className='dates'>{props.dates}</p></>
+    <>
+      <img className='image' src={`${process.env.PUBLIC_URL}/symbols/${props.name}.png`} />
+      <h2 className="name">{props.name}</h2>
+      <h3 className='symbol'>{props.symbol}</h3>
+      <p className='dates'>{props.dates}</p>
+    </>
   </div>);
 }
